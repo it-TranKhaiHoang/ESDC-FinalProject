f12a4d528359202cd5add4e433560be0e8c7a9ef
Tran Khai Hoang <hoangbo74@gmail.com>
2023-04-06 17:04:42 +0700

    authenzation - check member

---
+++ src/resources/auth/role.js
@@ -0,0 +1,10 @@
+const role = (role) => {
+    return (req,res,next) => {
+        if (role.includes(req.user.role)) {
+            return res.status(403).flash("error", "Access denied").redirect();
+        }
+        next();
+    };
+};
+
+module.exports = role;

52cd4b2af73c72ebb60c440330cf53ffd900f568
Tran Khai Hoang <hoangbo74@gmail.com>
2023-04-06 21:34:37 +0700

    update Comment model

--- src/resources/models/Comment.js
+++ src/resources/models/Comment.js
@@ -1,13 +1,16 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const Comment = new Schema({
-    author: { type: Schema.Types.ObjectId, ref: 'Member'},
-    body: { type: String }
-    
-},
-{
-    timestamps: true
-})
+const Comment = new Schema(
+    {
+        task: { type: Schema.Types.ObjectId, ref: 'Task' },
+        author: { type: Schema.Types.ObjectId, ref: 'Member' },
+        body: { type: String },
+        status: { type: String },
+    },
+    {
+        timestamps: true,
+    },
+);
 
 module.exports = mongoose.model('Comment', Comment);

27fff095378670dade716f506b58526f2561f5ad
Tran Khai Hoang <hoangbo74@gmail.com>
2023-04-06 16:31:40 +0700

    setup routers

---
+++ src/resources/routers/LogRouter.js
@@ -0,0 +1,5 @@
+const express = require("express");
+const router = express.Router();
+const LogController = require("../controllers/LogController");
+
+module.exports = router;
---
+++ src/resources/routers/MemberRouter.js
@@ -0,0 +1,5 @@
+const express = require("express");
+const router = express.Router();
+const MemberController = require("../controllers/MemberController");
+
+module.exports = router;

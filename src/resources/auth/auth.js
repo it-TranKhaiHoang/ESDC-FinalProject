9767f5d07f1672007df6b916c7554fd0b3e3df81
Tran Khai Hoang <hoangbo74@gmail.com>
2023-04-06 17:01:09 +0700

    authenzation - check login

---
+++ src/resources/auth/auth.js
@@ -0,0 +1,20 @@
+const jwt = require("jsonwebtoken");
+
+const auth = (req, res, next) => {
+    const token = req.headers.authorization.split(' ')[1];
+    if (!token) {
+        res.status(401).flash("error", "Access denied. No token provided")
+        return res.redirect();
+    }
+    jwt.verify(token, process.env.JWT_SECRET, (err, decodedToken) => {
+        if (err) {
+            res.status(401).flash("error", "Invalid Token");
+            return res.redirect();
+        } else {
+            req.decodedToken = decodedToken;
+            next();
+        }
+    })
+};
+
+module.exports = auth;

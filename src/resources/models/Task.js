7a8b473d9aa31b020ba51e99847551b710317bc5
Tran Khai Hoang <hoangbo74@gmail.com>
2023-04-07 11:09:01 +0700

    update Task model

--- src/resources/models/Task.js
+++ src/resources/models/Task.js
@@ -4,19 +4,15 @@ const Schema = mongoose.Schema;
 const Task = new Schema({
     name: { type: String, required: true },
     project: { type: Schema.Types.ObjectId, ref: 'Project'},
-    type: { type: String, required: true },
     status: { type: String, required: true },
-    summary: { type: String, required: true },
     description: { type: String },
     members: [{ type: Schema.Types.ObjectId, ref: 'Member'}],
-    labels: [{ type: String }],
     start_date: { type: Date, required: true },
     end_date: { type: Date, required: true },
-    reporter: { type: Schema.Types.ObjectId, ref: 'Member'},
     attachments: [{ type: String }],
     linked: { type: String },
-    comments: { type: Schema.Types.ObjectId, ref: 'Comment'},
-    logs: { type: Schema.Types.ObjectId, ref: 'Log' }
+    comments: [{ type: Schema.Types.ObjectId, ref: 'Comment'}],
+    logs: [{ type: Schema.Types.ObjectId, ref: 'Log' }]
 },
 {
     timestamps: true

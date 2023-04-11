08fdc546fa07f9f5f5f0b8c0588b2f6679f887f3
Tran Khai Hoang <hoangbo74@gmail.com>
2023-04-11 16:58:35 +0700

    fix auth

--- EzGroup/src/resources/routers/TaskRouter.js
+++ EzGroup/src/resources/routers/TaskRouter.js
@@ -5,12 +5,12 @@ const { upload } = require('../middleware/multer');
 const auth = require('../auth/auth');
 const role = require('../auth/role');
 
-router.post('/create', auth, role(['leader']), upload.array('files'), TaskController.postCreateTask);
-router.get('/complete/:id', auth, role(['leader']), TaskController.getComplete);
+router.post('/create', auth, upload.array('files'), TaskController.postCreateTask);
+router.get('/complete/:id', auth, TaskController.getComplete);
 router.get('/list', auth, TaskController.getTaskByMember);
 router.get('/progressing/:id', auth, TaskController.getProgressing);
 router.get('/pending/:id', auth, TaskController.getPending);
-router.get('/cancel/:id', auth, role(['leader']), TaskController.getCancel);
-router.get('/unqualified/:id', auth, role(['leader']), TaskController.getUnqualified);
+router.get('/cancel/:id', auth, TaskController.getCancel);
+router.get('/unqualified/:id', auth, TaskController.getUnqualified);
 router.get('/:id', auth, TaskController.getDetail);
 module.exports = router;

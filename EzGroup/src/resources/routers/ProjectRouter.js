d0617517195e7f3c8b63bd3b7349c8338211d509
Tran Khai Hoang <hoangbo74@gmail.com>
2023-04-11 23:34:35 +0700

    fix auth

--- EzGroup/src/resources/routers/ProjectRouter.js
+++ EzGroup/src/resources/routers/ProjectRouter.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const ProjectController = require('../controllers/ProjectController');
-
-router.get('/management', ProjectController.getProjectManagement);
-router.get('/list', ProjectController.getList);
-router.post('/create', ProjectController.postCreateProject);
-router.get('/:id', ProjectController.getDetail);
+const auth = require('../auth/auth');
+router.get('/management', auth, ProjectController.getProjectManagement);
+router.get('/list', auth, ProjectController.getList);
+router.post('/create', auth, ProjectController.postCreateProject);
+router.get('/:id', auth, ProjectController.getDetail);
 module.exports = router;
